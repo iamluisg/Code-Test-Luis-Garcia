1d8947091335d63ea5b87850a6c267b877b8dbca
Luis Garcia <iam.luisg@gmail.com>
2018-12-19 09:33:58 -0600

    Initial commit

---
+++ Code Test Luis Garcia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Test Luis Garcia
+//
+//  Created by Luis Garcia on 12/19/18.
+//  Copyright © 2018 Luis Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
